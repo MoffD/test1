175b0f3da7df38a6514637f90f6e44defe2dcec2
Steven Marcus <slmarcus@yahoo.com>
2014-07-29 19:27:37 -0400

    added ennis.cpp

---
+++ ennis.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int a = 0;
+int b = 0;
+
+int main()
+{
+// int a=5;
+
+cout << "\n\nEnter the width of the rectangle: ";
+cin >> a;
+cout << "\nEnter the length of the rectangle: ";
+cin >> b;
+int c;
+c = a * b;
+
+cout << "The area of the rectangle is: " << c << "\n\n";
+
+return 0;
+}
